8cfda91824475e00d9ff4ea2834c00388fcb8353
marufmks <marufmks5556@gmail.com>
2025-01-17 19:25:16 +0600

    Input change and save added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,36 @@ registerBlockType( 'custom-guten-block/test-block', {
 	title: 'Basic Example',
 	icon: 'smiley',
 	category: 'layout',
-	edit: ( { className } ) => <div className={ className }>Hello World!</div>,
-	save: () => <div>Hello World!</div>,
+
+	attributes: {
+		content: {
+			type: 'array',
+			source: 'children',
+			selector: 'p',
+		},
+	},
+
+	edit: (props)=> {
+		console.log(props);
+		const { attributes: { content }, setAttributes, className } = props;
+		const onChangeContent = ( newContent ) => {
+			setAttributes( { content: newContent } );
+		};
+
+		return (
+			<div className={ className }>
+				<h2>Input Text Example</h2>
+				<p>
+					<input
+						type="text"
+						value={ content }
+						onChange={ ( event ) => onChangeContent( event.target.value ) }
+					/>
+				</p>
+			</div>
+		);
+	},
+	save: (props)=> {
+		return <p>{ props.attributes.content }</p>;
+	},
 } );
